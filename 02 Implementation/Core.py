0f34564c2e80164f6c790e266cab568b1347831c
Awall1302 <awall02@hotmail.co.uk>
2023-08-16 15:02:05 +0100

    Adding everything on my local machine

--- 02 Implementation/Core.py
+++ 02 Implementation/Core.py
@@ -2,7 +2,6 @@ from GUI import gui
 from DatabaseManager import databaseManager
 from InformationManager import informationManager
 
-
 class Core:
     def __init__(self):
         self.DM = databaseManager(self)
@@ -10,14 +9,25 @@ class Core:
         self.Gui = gui(self)
 
     def getLocation(self):
-        location = self.IM.getGPS()
-        return location
+        self.IM.getGPS()
 
     def getPhoto(self):
         self.IM.takePhoto()
 
     def CheckLoginInfo(self, data):
         return self.DM.CheckLoginInfo(data)
+
+    def getAdminIDs(self):
+        return self.DM.getAdminIDs()
+
+    def addEmployeeToDatabase(self, employeeData):
+        self.DM.addEmployeeToDatabase(employeeData)
+
+    def getEmployees(self, userLevel):
+        return self.DM.getEmployees(userLevel)
+
+
+        
 
 
 entryPoint = Core()
--- 02 Implementation/DatabaseManager.py
+++ 02 Implementation/DatabaseManager.py
@@ -4,9 +4,9 @@ import mysql.connector
 class databaseManager:
     def __init__(self, Core):
         core = Core
-        # db = self.connect()
-        # self.myCursor = db.cursor()
-        # self.Querie()
+        #db = self.connect()
+        #self.myCursor = db.cursor()
+        #self.Querie()
 
     def connect(self):
         db = mysql.connector.connect(
@@ -19,8 +19,51 @@ class databaseManager:
     def CheckLoginInfo(self, data):
         db = self.connect()
         myCursor = db.cursor()
-        myCursor.execute(f"SELECT Password FROM testing.logins WHERE ID = {data['ID']};")
-        password = myCursor.fetchone()
-        if password[0] == data["Password"]:
-            return True
-        return False
+        try:
+            myCursor.execute(f"SELECT Password, level FROM asp_assignment.employee_info WHERE EmployeeID = {data['ID']};")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+        info = myCursor.fetchone()
+        if info[0] == data["Password"]:
+            return [True, info[1]]
+        return [False, info[1]]
+
+    def getAdminIDs(self):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"SELECT EmployeeID, FullName FROM asp_assignment.employee_info Where Level > 1;")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+        return myCursor.fetchall()
+
+    def addEmployeeToDatabase(self, employeeData):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"INSERT INTO `asp_assignment`.`employee_info` (`FullName`, `Address`, `Postcode`, `Email`, `Telephone`, `DateOfBirth`, `StartDate`, `AdminID`, `Level`, `Password`) "
+                             f"VALUES ('{employeeData['FullName']}', '{employeeData['Address']}', '{employeeData['Postcode']}', '{employeeData['Email']}', '{employeeData['PhoneNo']}', '{employeeData['DoB']}', "
+                             f"'{employeeData['StartDate']}', '{employeeData['AdminID']}', '{employeeData['Level']}', 'Password');")
+            db.commit()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+    def getEmployees(self, userLevel):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"SELECT EmployeeID, FullName FROM asp_assignment.employee_info Where Level < {userLevel};")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return "An error occurred"
+
+        return myCursor.fetchall()

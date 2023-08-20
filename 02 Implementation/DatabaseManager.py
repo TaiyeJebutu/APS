5c202460d9dc500a524219d09b37e3e9b1f464f2
Awall1302 <54173919+Awall1302@users.noreply.github.com>
2023-08-20 17:36:35 +0100

    Alexs first branch (#4)
    * Adding everything on my local machine
    
    * After clicking on an employee in the list opens a new page and display all their info including pay details
    
    * Changed the design of Login screen
    
    * Allows user to change password after logging in

--- 02 Implementation/DatabaseManager.py
+++ 02 Implementation/DatabaseManager.py
@@ -4,9 +4,9 @@ import mysql.connector
 class databaseManager:
     def __init__(self, Core):
         core = Core
-        # db = self.connect()
-        # self.myCursor = db.cursor()
-        # self.Querie()
+        #db = self.connect()
+        #self.myCursor = db.cursor()
+        #self.Querie()
 
     def connect(self):
         db = mysql.connector.connect(
@@ -19,8 +19,79 @@ class databaseManager:
     def CheckLoginInfo(self, data):
         db = self.connect()
         myCursor = db.cursor()
-        myCursor.execute(f"SELECT Password FROM testing.logins WHERE ID = {data['ID']};")
-        password = myCursor.fetchone()
-        if password[0] == data["Password"]:
-            return True
-        return False
+        try:
+            myCursor.execute(f"SELECT Password, level FROM asp_assignment.employee_info WHERE EmployeeID = {data['ID']};")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+        info = myCursor.fetchone()
+        if info[0] == data["Password"]:
+            return [True, info[1]]
+        return [False, info[1]]
+
+    def getAdminIDs(self):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"SELECT EmployeeID, FullName FROM asp_assignment.employee_info Where Level > 1;")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+        return myCursor.fetchall()
+
+    def addEmployeeToDatabase(self, employeeData):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"INSERT INTO `asp_assignment`.`employee_info` (`FullName`, `Address`, `Postcode`, `Email`, `Telephone`, `DateOfBirth`, `StartDate`, `AdminID`, `Level`, `Password`) "
+                             f"VALUES ('{employeeData['FullName']}', '{employeeData['Address']}', '{employeeData['Postcode']}', '{employeeData['Email']}', '{employeeData['PhoneNo']}', '{employeeData['DoB']}', "
+                             f"'{employeeData['StartDate']}', '{employeeData['AdminID']}', '{employeeData['Level']}', 'Password');")
+
+            myCursor.execute(f"INSERT INTO asp_assignment.pay_details (EmployeeID, PaymentDate, AnnualSalary, TaxCode, MonthlySalary, Pension) "
+                             f"SELECT EmployeeID, '{employeeData['PaymentDate']}', {employeeData['AnnualSalary']}, '{employeeData['TaxCode']}', {employeeData['MonthlySalary']}, {employeeData['Pension']} "
+                             f"FROM asp_assignment.employee_info "
+                             f"WHERE FullName='{employeeData['FullName']}' AND Level = {employeeData['Level']};")
+            db.commit()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return False
+
+    def getEmployees(self, userLevel):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"SELECT EmployeeID, FullName FROM asp_assignment.employee_info Where Level < {userLevel};")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return "An error occurred"
+
+        return myCursor.fetchall()
+
+    def getEmployeeInfo(self, employeeID):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"SELECT * from asp_assignment.employee_info e, asp_assignment.pay_details p where e.EmployeeID = {employeeID} and p.EmployeeID = {employeeID};")
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return "An error occurred"
+
+        return myCursor.fetchone()
+
+    def updatePassword(self, newPassword, userID):
+        db = self.connect()
+        myCursor = db.cursor()
+        try:
+            myCursor.execute(f"UPDATE asp_assignment.employee_info SET Password = '{newPassword}' Where EmployeeID = {userID};")
+            db.commit()
+
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return "An error occurred"
--- 02 Implementation/GUI.py
+++ 02 Implementation/GUI.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask.views import MethodView
 
 app = Flask(__name__)
@@ -6,26 +6,68 @@ app = Flask(__name__)
 class gui(MethodView):
     def __init__(self, Core):
         self.core = Core
-        app.add_url_rule("/form", "form", self.form)
+        app.add_url_rule("/form", "form", self.form, methods=['POST', 'GET'])
+        app.add_url_rule("/test", "test", self.test)
         app.add_url_rule("/data/", "data", self.data, methods=['POST', 'GET'])
-        app.add_url_rule("/photo" 'photo', self.photo)
+        app.add_url_rule("/CreateEmployee/", "CreateEmployee", self.createEmployee, methods=['POST', 'GET'])
+        app.add_url_rule("/EmployeeInfo/", "EmployeeInfo", self.employeeInfo, methods=['POST', 'GET'])
+        app.add_url_rule("/ChangePassword/", "ChangePassword", self.changePassword, methods=['POST', 'GET'])
         app.run(host='0.0.0.0', port=80)
+        self.loggedIn = False
+        self.userLevel = 0
+        self.userID = 0
 
     def form(self):
-        return render_template('index.html')
+        if request.method == "POST":
+            form_data = {"ID": request.form.get("ID"), "Password": request.form.get("Password")}
+            self.userID = form_data['ID']
+            PasswordAndLevel = self.core.CheckLoginInfo(form_data)
+            self.userLevel = PasswordAndLevel[1]
+            if PasswordAndLevel[0]:
+                self.loggedIn = True
+                if self.userLevel <= 1:
+                    return redirect("/test")
+                else:
+                    return redirect('/data')
+            else:
+                return "Incorrect Username or Password"
+        else:
+            return render_template('index.html')
+
+    def test(self):
+        return render_template('test.html')
 
     def data(self):
-        # if request.method == 'GET':
-        #     return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-        # if request.method == 'POST':
-        #     form_data = {"ID" : request.form.get("ID"), "Password" : request.form.get("Password")}
-        #     if self.core.CheckLoginInfo(form_data):
-        #         return render_template('data.html',form_data = form_data)
-        #     else:
-        #         return "Incorrect Username or Password"
-        location = self.core.getLocation()
-        form_data = {"ID": request.form.get("ID"),
-                     "Password": request.form.get("Password"),
-                     "Location": location}
+        selectedableEmployees = self.core.getEmployees(self.userLevel)
+        return render_template("data.html", employees = selectedableEmployees)
 
-        return render_template('data.html', form_data=form_data)
+
+    def createEmployee(self):
+        if not self.loggedIn:
+            return redirect("/form")
+
+        if request.method == "POST":
+            form_data = {"FullName" : request.form.get("FullName"), "Address": request.form.get("Address"), "Postcode": request.form.get("Postcode"), "Email": request.form.get("Email"),
+                         "PhoneNo": request.form.get("PhoneNo"), "DoB": request.form.get("DoB"), "StartDate": request.form.get("StartDate"), "AdminID": request.form.get("AdminID"),
+                         "Level": request.form.get("Level"), "PaymentDate": request.form.get("PaymentDate"), "AnnualSalary": request.form.get("AnnualSalary"), "TaxCode": request.form.get("TaxCode"),
+                         "MonthlySalary": request.form.get("MonthlySalary"), "Pension": request.form.get("Pension")}
+            self.core.addEmployeeToDatabase(form_data)
+            return redirect("/data")
+        else:
+            AdminIDs = self.core.getAdminIDs()
+            return render_template("CreateEmployee.html", AdminIDs=AdminIDs)
+
+    def employeeInfo(self):
+        employeeID = request.form.get("Employee")
+        allInfo = self.core.getEmployeeInfo(employeeID[1])
+        return render_template("EmployeeInfo.html", info=allInfo)
+
+    def changePassword(self):
+        if request.method == "POST":
+            newPassword = request.form.get("NewPassword")
+            self.core.updatePassword(newPassword, self.userID)
+            return redirect("/data")
+        else:
+            return render_template("ChangePassword.html")
+
+

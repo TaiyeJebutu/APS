0f84acbee594fc8f2ab3783c60b974b9ffcbed35
Awall1302 <54173919+Awall1302@users.noreply.github.com>
2023-08-29 11:19:38 +0100

    Alexs first branch (#6)

--- 02 Implementation/GUI.py
+++ 02 Implementation/GUI.py
@@ -1,7 +1,6 @@
-
-
-import cv2
+import cv2, threading
 from flask import Flask, render_template, request, redirect, Response
+
 from flask.views import MethodView
 
 from TakePhoto import takePhoto
@@ -14,57 +13,67 @@ class gui(MethodView):
     formData = None
     def __init__(self, Core):
         self.core = Core
+        self.loggedIn = False
+        self.userLevel = 0
+        self.loggedInUserID = 0
+        self.selectedEmployeeID = 0
+        self.startGui()
+
+    def startGui(self):
         app.add_url_rule("/form", "form", self.form, methods=['POST', 'GET'])
-        app.add_url_rule("/test", "test", self.test)
         app.add_url_rule("/data/", "data", self.data, methods=['POST', 'GET'])
         app.add_url_rule("/CreateEmployee/", "CreateEmployee", self.createEmployee, methods=['POST', 'GET'])
         app.add_url_rule("/EmployeeInfo/", "EmployeeInfo", self.employeeInfo, methods=['POST', 'GET'])
         app.add_url_rule("/ChangePassword/", "ChangePassword", self.changePassword, methods=['POST', 'GET'])
+        app.add_url_rule("/EditEmployeeInfo", "EditEmployeeInfo", self.editEmployeeInfo, methods=['POST', 'GET'])
         app.add_url_rule("/photoPage/", 'photoPage', self.photoPage, methods=['POST', 'GET'])
         app.add_url_rule("/savePhoto/", 'savePhoto', self.savePhoto, methods=['POST', 'GET'])
         app.add_url_rule("/video_feed/", 'video_feed', self.video_feed, methods=['POST', 'GET'])
         app.run(host='0.0.0.0', port=80)
-        self.loggedIn = False
-        self.userLevel = 0
-        self.userID = 0
 
     def form(self):
         if request.method == "POST":
             form_data = {"ID": request.form.get("ID"), "Password": request.form.get("Password")}
-            self.userID = form_data['ID']
+            self.loggedInUserID = form_data['ID']
             PasswordAndLevel = self.core.CheckLoginInfo(form_data)
             self.userLevel = PasswordAndLevel[1]
             if PasswordAndLevel[0]:
                 self.loggedIn = True
+                self.core.getLocation(self.loggedInUserID)
                 if self.userLevel <= 1:
-                    return redirect("/test")
+                    try:
+                        return redirect("/EmployeeInfo")
+                    except Exception as e:
+                        print(f"An error occurred: {e}")
+                        return redirect("/EmployeeInfo")
                 else:
                     return redirect('/data')
             else:
                 return "Incorrect Username or Password"
         else:
+            if self.loggedIn:
+                self.loggedIn = False
+                self.core.stopLocation()
             return render_template('index.html')
-
-
-    def test(self):
-        return render_template('test.html')
 
     def data(self):
         selectedableEmployees = self.core.getEmployees(self.userLevel)
         return render_template("data.html", employees=selectedableEmployees)
 
     def photoPage(self):
-        return render_template('photoPage.html')
+        if self.userLevel <= 1:
+            userAccessibleTabs = [["Home", "/EmployeeInfo"], ["Change Password", "/ChangePassword"]]
+            return render_template('PhotoPageV2.html', tabs=userAccessibleTabs)
+        else:
+            userAccessibleTabs = [["Home", "/data"], ["Change Password", "/ChangePassword"], ["Create Employee", "/CreateEmployee"]]
+            return render_template('PhotoPageV2.html', tabs=userAccessibleTabs)
 
     def savePhoto(self):
         takePhoto.savePhoto()
-        return self.data()
-
-
+        return redirect('/data')
 
     def video_feed(self):
-        return Response(takePhoto().getPhoto(self.userID), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+        return Response(takePhoto().getPhoto(self.loggedInUserID), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
     def createEmployee(self):
@@ -83,20 +92,59 @@ class gui(MethodView):
             return render_template("CreateEmployee.html", AdminIDs=AdminIDs)
 
     def employeeInfo(self):
-        employeeID = request.form.get("Employee")
+        if not self.loggedIn:
+            return redirect("/form")
 
-        allInfo = {"db_info":self.core.getEmployeeInfo(employeeID[1])}
-        allInfo["location"] = self.core.getLocation()
+        if request.method == "POST":
+            try:
+                employeeID = request.form.get("Employee")
+                self.selectedEmployeeID = employeeID[1]
+                personalInfo, locationInfo = self.core.getEmployeeInfo(employeeID[1])
+                return render_template("EmployeeInfo.html", personalInfo=personalInfo, locationInfo=locationInfo,
+                                       adminPage=[1])
+            except Exception as e:
+                print(f"An error occurred {e}")
+                return redirect("/data")
 
-
-        return render_template("EmployeeInfo.html", info=allInfo)
+        elif request.method == "GET":
+            if not self.loggedIn:
+                return redirect("/form")
+            personalInfo, locationInfo = self.core.getEmployeeInfo(self.loggedInUserID)
+            return render_template("EmployeeInfo.html", personalInfo=personalInfo, locationInfo=locationInfo,
+                                   adminPage=[])
 
     def changePassword(self):
         if request.method == "POST":
             newPassword = request.form.get("NewPassword")
-            self.core.updatePassword(newPassword, self.userID)
+            self.core.updatePassword(newPassword, self.loggedInUserID)
+            if self.userLevel <= 1:
+                return redirect("/EmployeeInfo")
+            else:
+                return redirect("/data")
+        else:
+            if self.userLevel <= 1:
+                usersAccessibleTabs = [["Home", "/EmployeeInfo"], ["Take Photo", "/photoPage"]]
+                return render_template("ChangePassword.html", tabs=usersAccessibleTabs)
+            else:
+                usersAccessibleTabs = [["Home", "/data"], ["Take Photo", "/photoPage"], ["Create Employee", "/CreateEmployee"]]
+                return render_template("ChangePassword.html", tabs=usersAccessibleTabs)
+
+    def editEmployeeInfo(self):
+        if not self.loggedIn:
+            return redirect("/form")
+
+        if request.method == "POST":
+            form_data = {"FullName": request.form.get("FullName"), "Address": request.form.get("Address"),
+                         "Postcode": request.form.get("Postcode"), "Email": request.form.get("Email"),
+                         "PhoneNo": request.form.get("Telephone"), "DateOfBirth": request.form.get("DoB"),
+                         "StartDate": request.form.get("StartDate"), "AdminID": request.form.get("AdminID"),
+                         "Level": request.form.get("Level"), "PaymentDate": request.form.get("PaymentDate"),
+                         "AnnualSalary": request.form.get("AnnualSalary"), "TaxCode": request.form.get("TaxCode"),
+                         "MonthlySalary": request.form.get("MonthlySalary"), "Pension": request.form.get("Pension")}
+            self.core.updateEmployeeInfo(self.selectedEmployeeID, form_data)
             return redirect("/data")
         else:
-            return render_template("ChangePassword.html")
+            AdminIDs = self.core.getAdminIDs()
+            return render_template("/EditEmployeeInfo.html", AdminIDs=AdminIDs)
 
 
--- 02 Implementation/InformationManager.py
+++ 02 Implementation/InformationManager.py
@@ -1,15 +1,34 @@
 from GetLocation import getLocation
 from TakePhoto import takePhoto
+from datetime import datetime
+import time, threading
+
 
 class informationManager:
     def __init__(self, Core):
         self.core = Core
-        self.location = getLocation()
         self.photo = takePhoto()
+        self.location = getLocation()
+        self.locationThread = None
+        self.exit_event = threading.Event()
 
-    def getGPS(self):
-        g = self.location.getCurrentLocation()
-        return g
+    def createLocationThread(self, employeeID):
+        self.locationThread = threading.Thread(target=self.startLocationThread, daemon=True, args=(employeeID))
+        self.locationThread.setName("Location_thread")
+        self.locationThread.start()
+
+    def startLocationThread(self, employeeID):
+        while True:
+            if self.exit_event.is_set():
+                self.exit_event.clear()
+                break
+            location = self.location.getCurrentLocation()
+            dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.core.addLocation(location, dt, employeeID)
+            time.sleep(10)
+
+    def stopLocationThread(self):
+        self.exit_event.set()
 
     def takePhoto(self,userID):
         return self.photo.getPhoto(userID)
